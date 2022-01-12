008d0475e0270b608e8153da0efbe61e760017a3
vibhishan kumar <47953033+vibhishankumar@users.noreply.github.com>
2022-01-12 10:04:46 +0530

    Create leftViewBinary.java

---
+++ leftViewBinary.java
@@ -0,0 +1,67 @@
+import java.util.*;
+
+public class PrintLeftView {
+    // Binary tree node
+    private static class Node {
+        int data;
+        Node left, right;
+
+        public Node(int data)
+        {
+            this.data = data;
+            this.left = null;
+            this.right = null;
+        }
+    }
+
+    // function to print left view of binary tree
+    private static void printLeftView(Node root)
+    {
+        if (root == null)
+            return;
+
+        Queue<Node> queue = new LinkedList<>();
+        queue.add(root);
+
+        while (!queue.isEmpty()) {
+            // number of nodes at current level
+            int n = queue.size();
+
+            // Traverse all nodes of current level
+            for (int i = 1; i <= n; i++) {
+                Node temp = queue.poll();
+
+                // Print the left most element at
+                // the level
+                if (i == 1)
+                    System.out.print(temp.data + " ");
+
+                // Add left node to queue
+                if (temp.left != null)
+                    queue.add(temp.left);
+
+                // Add right node to queue
+                if (temp.right != null)
+                    queue.add(temp.right);
+            }
+        }
+    }
+
+    // Driver code
+    public static void main(String[] args)
+    {
+        // construct binary tree as shown in
+        // above diagram
+        Node root = new Node(1);
+        root.left = new Node(2);
+        root.right = new Node(3);
+        root.left.left = new Node(4);
+        root.left.right = new Node(5);
+        // root.right.right = new Node(15);
+        // root.right.left = new Node(12);
+        // root.right.right.left = new Node(14);
+
+        printLeftView(root);
+    }
+}
+//o/p  1 2 4 
